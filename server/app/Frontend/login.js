589a3c2efabf1ba6e424e01e2c19f0b946553b7e
Georg Rohrhofer <georg.rohrhofer@outlook.com>
2024-02-09 23:05:00 +0100

    update jquery version in login.js

--- server/app/Frontend/login.js
+++ server/app/Frontend/login.js
@@ -1,5 +1,5 @@
 var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.6.3.min.js'; // Check https://jquery.com/ for the current version
+script.src = 'https://code.jquery.com/jquery-3.7.1.min.js'; // Check https://jquery.com/ for the current version
 document.getElementsByTagName('head')[0].appendChild(script);
 
 function checkLogin()
