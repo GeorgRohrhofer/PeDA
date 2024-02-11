806701d4176d245eedddcef4d17cdaea8d5c76eb
Georg Rohrhofer <>
2024-02-11 02:40:21 +0100

    add create statements for database and testdata for user and newsletter

--- flyway/sql/V1__Initial.sql
+++ flyway/sql/V1__Initial.sql
@@ -1,1 +1,36 @@
---TODO: Create Tables
+create table users
+(
+	user_id int primary key;
+	username varchar(255);
+	email varchar(255);
+	password varchar(255);
+);
+
+create table newsletter_entry
+(
+	entry_id int primary key;
+	entry_date timestamp;
+	entry_title varchar(255);
+	entry_text text;
+);
+
+create table tournament
+(
+	tournament_id int primary key;
+	tournament_name varchar(255);
+	tournament_date timestamp;
+	tournament_title varchar(255);
+	tournament_desc text;
+);
+
+create table match
+(
+	match_id int primary key;
+	user1 int references users;
+	user2 int references users;
+	sets_user1 int;
+	sets_user2 int;
+	tournament_id int references tournament;
+);
+
+
